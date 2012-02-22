7188b50f6fc3a8c558bd0a3b0b2811843b299f80
Simone Bordet <simone.bordet@gmail.com>
2012-02-22 14:13:24 +0100

    Documentation and javadocs.

--- npn-boot/src/main/java/org/eclipse/jetty/npn/NextProtoNego.java
+++ npn-boot/src/main/java/org/eclipse/jetty/npn/NextProtoNego.java
@@ -23,8 +23,69 @@ import java.util.WeakHashMap;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLSocket;
 
+/**
+ * <p>{@link NextProtoNego} provides an API to applications that want to make use of the
+ * <a href="http://technotes.googlecode.com/git/nextprotoneg.html">Next Protocol Negotiation</a>.</p>
+ * <p>The NPN extension is only available when using the TLS protocol, therefore applications must
+ * ensure that the TLS protocol is used:</p>
+ * <pre>
+ * SSLContext context = SSLContext.getInstance("TLSv1");
+ * </pre>
+ * <p>Refer to the
+ * <a href="http://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#SSLContext">list
+ * of standard SSLContext protocol names</a> for further information on TLS protocol versions supported.</p>
+ * <p>Applications must register instances of either {@link SSLSocket} or {@link SSLEngine} with a
+ * {@link ClientProvider} or with a {@link ServerProvider}, depending whether they are on client or
+ * server side.</p>
+ * <p>The NPN implementation will invoke the provider callbacks to allow applications to interact
+ * with the negotiation of the next protocol.</p>
+ * <p>Client side typical usage:</p>
+ * <pre>
+ * SSLSocket sslSocket = ...;
+ * NextProtoNego.put(sslSocket, new NextProtoNego.ClientProvider()
+ * {
+ *     &#64;Override
+ *     public boolean supports()
+ *     {
+ *         return true;
+ *     }
+ *
+ *     &#64;Override
+ *     public String selectProtocol(List&lt;String&gt; protocols)
+ *     {
+ *         return protocols.get(0);
+ *     }
+ *  });
+ * </pre>
+ * <p>Server side typical usage:</p>
+ * <pre>
+ * SSLSocket sslSocket = ...;
+ * NextProtoNego.put(sslSocket, new NextProtoNego.ServerProvider()
+ * {
+ *     &#64;Override
+ *     public List<String> protocols()
+ *     {
+ *         return Arrays.asList("http/1.1");
+ *     }
+ *
+ *     &#64;Override
+ *     public void protocolSelected(String protocol)
+ *     {
+ *         System.out.println("Protocol Selected is: " + protocol);
+ *     }
+ *  });
+ * </pre>
+ * <p>There is no need to unregister {@link SSLSocket} or {@link SSLEngine} instances, as they
+ * are kept in a {@link WeakHashMap} and will be garbage collected when the application does not
+ * hard reference them anymore.</p>
+ * <p>In order to help application development, you can set the {@link NextProtoNego#debug} field
+ * to {@code true} to have debug code printed to {@link System#err}.</p>
+ */
 public class NextProtoNego
 {
+    /**
+     * <p>Enables debug logging on {@link System#err}.</p>
+     */
     public static boolean debug = false;
 
     private static Map<Object, Provider> objects = Collections.synchronizedMap(new WeakHashMap<Object, Provider>());
@@ -33,30 +94,58 @@ public class NextProtoNego
     {
     }
 
+    /**
+     * <p>Registers a SSLSocket with a provider.</p>
+     *
+     * @param socket the socket to register with the provider
+     * @param provider the provider to register with the socket
+     */
     public static void put(SSLSocket socket, Provider provider)
     {
         objects.put(socket, provider);
     }
 
+    /**
+     * @param socket a socket registered with {@link #put(SSLSocket, Provider)}
+     * @return the provider registered with the given socket
+     */
     public static Provider get(SSLSocket socket)
     {
         return objects.get(socket);
     }
 
+    /**
+     * <p>Registers a SSLEngine with a provider.</p>
+     *
+     * @param engine the engine to register with the provider
+     * @param provider the provider to register with the engine
+     */
     public static void put(SSLEngine engine, Provider provider)
     {
         objects.put(engine, provider);
     }
 
+    /**
+     *
+     * @param engine an engine registered with {@link #put(SSLEngine, Provider)}
+     * @return the provider registered with the given engine
+     */
     public static Provider get(SSLEngine engine)
     {
         return objects.get(engine);
     }
 
+    /**
+     * <p>Base, empty, interface for providers.</p>
+     */
     public interface Provider
     {
     }
 
+    /**
+     * <p>The client-side provider interface that applications must implement to interact
+     * with the negotiation of the next protocol.</p>
+     */
     public interface ClientProvider extends Provider
     {
         /**
@@ -82,6 +171,10 @@ public class NextProtoNego
         public String selectProtocol(List<String> protocols);
     }
 
+    /**
+     * <p>The server-side provider interface that applications must implement to interact
+     * with the negotiation of the next protocol.</p>
+     */
     public interface ServerProvider extends Provider
     {
         /**
