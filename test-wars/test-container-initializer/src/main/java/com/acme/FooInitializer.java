9318694097854067c115d4b98b3b710bf185b2ef
Jan Bartel <janb@intalio.com>
2012-07-12 16:33:53 +1000

    384280 Implement preliminary ServletRegistrations

--- test-wars/test-container-initializer/src/main/java/com/acme/FooInitializer.java
+++ test-wars/test-container-initializer/src/main/java/com/acme/FooInitializer.java
@@ -2,6 +2,7 @@ package com.acme;
 
 import java.util.Set;
 import java.util.ArrayList;
+import javax.servlet.ServletRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.HandlesTypes;
 import javax.servlet.ServletContainerInitializer;
@@ -13,5 +14,7 @@ public class FooInitializer implements ServletContainerInitializer
     public void onStartup(Set<Class<?>> classes, ServletContext context)
     {
         context.setAttribute("com.acme.Foo", new ArrayList<Class>(classes));
+        ServletRegistration.Dynamic reg = context.addServlet("AnnotationTest", "com.acme.AnnotationTest");
+        context.setAttribute("com.acme.AnnotationTest.complete", (reg == null));
     }
 }
