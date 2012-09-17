661645e96de37a5fa61d25e9422a9c23d19ffc24
Jan Bartel <janb@intalio.com>
2012-09-18 09:24:39 +1000

    389670 ensure classes from overlayed wars are scanned

--- jetty-maven-plugin/src/main/java/org/mortbay/jetty/plugin/MavenAnnotationConfiguration.java
+++ jetty-maven-plugin/src/main/java/org/mortbay/jetty/plugin/MavenAnnotationConfiguration.java
@@ -1,18 +1,15 @@
 package org.mortbay.jetty.plugin;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.jetty.annotations.AbstractDiscoverableAnnotationHandler;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.annotations.AnnotationParser;
 import org.eclipse.jetty.annotations.AnnotationParser.DiscoverableAnnotationHandler;
 import org.eclipse.jetty.annotations.ClassNameResolver;
-import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.DiscoveredAnnotation;
 import org.eclipse.jetty.webapp.MetaData;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -49,28 +46,43 @@ public class MavenAnnotationConfiguration extends AnnotationConfiguration
 
             for (File f:jwac.getClassPathFiles())
             {
+                //scan the equivalent of the WEB-INF/classes directory that has been synthesised by the plugin
                 if (f.isDirectory() && f.exists())
                 {
-                    parser.parse(Resource.newResource(f.toURL()), 
-                                new ClassNameResolver()
-                    {
-                        public boolean isExcluded (String name)
-                        {
-                            if (context.isSystemClass(name)) return true;
-                            if (context.isServerClass(name)) return false;
-                            return false;
-                        }
-
-                        public boolean shouldOverride (String name)
-                        {
-                            //looking at webapp classpath, found already-parsed class of same name - did it come from system or duplicate in webapp?
-                            if (context.isParentLoaderPriority())
-                                return false;
-                            return true;
-                        }
-                    });
-                }                
+                    doParse(context, parser, Resource.newResource(f.toURL()));
+                }
+            }
+            
+            //if an actual WEB-INF/classes directory also exists (eg because of overlayed wars) then scan that
+            //too
+            Resource classesDir = context.getWebInf().addPath("classes/");
+            if (classesDir.exists())
+            {
+                    doParse(context, parser, classesDir);
             }
         }
+    }
+    
+    
+    public void doParse (final WebAppContext context, final AnnotationParser parser, Resource resource)
+    throws Exception
+    { 
+            parser.parse(resource, new ClassNameResolver()
+            {
+                public boolean isExcluded (String name)
+                {
+                    if (context.isSystemClass(name)) return true;
+                    if (context.isServerClass(name)) return false;
+                    return false;
+                }
+
+                public boolean shouldOverride (String name)
+                {
+                    //looking at webapp classpath, found already-parsed class of same name - did it come from system or duplicate in webapp?
+                    if (context.isParentLoaderPriority())
+                        return false;
+                    return true;
+                }
+            });            
     }
 }
